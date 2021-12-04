dd50b9ea90af1473db5e18f2fdd448e3b4d04b59
Thundo <thundo@protonmail.com>
2021-12-05 00:24:03 +0100

    Add local repo files

---
+++ src/AreYouDumb.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace AYD
+{
+    public partial class AreYouDumb : Window
+    {
+        static readonly Random rnd = new();
+        public AreYouDumb()
+        {
+            InitializeComponent();
+        }
+        private void Button1Click(object sender, RoutedEventArgs e)
+        {
+            Button1.Visibility = Visibility.Hidden;
+            Button2.Visibility = Visibility.Hidden;
+            TextBlock1.Visibility = Visibility.Hidden;
+            TextBlock2.Visibility = Visibility.Visible;
+        }
+        private void Button2Click(object sender, RoutedEventArgs e)
+        {
+            Button2.Margin = new(rnd.Next(20, 300), rnd.Next(-50, 270), 0, 0);
+        }
+    }
+}
